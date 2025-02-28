687d8baa89e410ea69d7b7ddca7b6f30df59cccc
Emanuel-Boaventura <emanuelbm.dev@gmail.com>
2025-02-28 18:02:32 -0400

    feat: update qrcode and change time limit

--- constants/game.ts
+++ constants/game.ts
@@ -12,7 +12,7 @@ export interface Card {
   matched: boolean
 }
 
-export const TIME_LIMIT = 30 // seconds
+export const TIME_LIMIT = 40 // seconds
 export const BLOCK_MOVE_TIME = 1000 * 0.5 // seconds
 export const INITIAL_SHOW_CARDS_TIME = 1000 * 2 // seconds
 export const MIN_CARDS = 4 // minimum number of cards
